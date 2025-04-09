599e2ad28422bb47b13eba5eff31f04856102ba7
LilianBarahona <163463816+LilianBarahona@users.noreply.github.com>
2025-04-09 00:29:15 -0600

    eventosp.js

---
+++ data/pages/eventosp.js
@@ -0,0 +1,38 @@
+export const eventosP = [
+    {
+        titulo: '¡Liquidación de Verano! (Pasado)',
+        descripcion: 'Grandes descuentos en ropa ligera, trajes de baño y accesorios para disfrutar de la temporada.',
+        fecha: '30 de Marzo de 2025',
+        mes: 'Marzo'
+    },
+    {
+        titulo: 'Especial del Día de la Mujer (Pasado)',
+        descripcion: 'Celebra el Día Internacional de la Mujer con ofertas especiales.',
+        fecha: '8 de Marzo de 2025',
+        mes: 'Marzo'
+    },
+    {
+        titulo: 'Día del Amor y la Amistad(Pasado)',
+        descripcion: 'Disfruta de nuestras promociones especiales durante todo el mes de febrero.',
+        fecha: '14 de Febrero de 2025',
+        mes: 'Febrero'
+    },
+    {
+        titulo: 'Ofertas de Inicio de Año (Pasado)',
+        descripcion: 'Comienza el año con los mejores descuentos en una gran variedad de productos.',
+        fecha: '15 de Enero de 2025',
+        mes: 'Enero'
+    },
+    {
+        titulo: 'Celebraciones de Fin de Año (Pasado)',
+        descripcion: 'Revive los mejores momentos de nuestras ofertas de fin de año.',
+        fecha: '28 de Diciembre de 2024',
+        mes: 'Diciembre'
+    },
+    {
+        titulo: 'Black Friday Extendido (Pasado)',
+        descripcion: 'Aprovecha los descuentos increíbles de nuestro Black Friday extendido.',
+        fecha: '25 de Noviembre de 2024',
+        mes: 'Noviembre'
+    }
+];
