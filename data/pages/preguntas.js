8f59dce06182bce0255d0161065a7065ce16b32c
Jared <f7.z7ro@gmail.com>
2025-04-09 01:02:24 -0600

    Updated info

--- data/pages/preguntas.js
+++ data/pages/preguntas.js
@@ -10,7 +10,7 @@ export const Preguntas = {
         },
         {
             pregunta: "¿Cómo se seleccionan las prendas para exhibir?",
-            respuesta: "¿Cómo se seleccionan las prendas para exhibir? R: Seleccionamos nuestra colección cuidadosamente, priorizando la creatividad, la calidad y las tendencias. Nuestro equipo selecciona piezas que destacan por su diseño y confección únicos."
+            respuesta: "¿Cómo se seleccionan las prendas para exhibir? Seleccionamos nuestra colección cuidadosamente, priorizando la creatividad, la calidad y las tendencias. Nuestro equipo selecciona piezas que destacan por su diseño y confección únicos."
         },
         {
             pregunta: "¿Con qué frecuencia se actualizan los eventos en el sitio web?",
