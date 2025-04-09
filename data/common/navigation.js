1eba615fa733d3b48496ae5ce5da35a3d0194c74
LuisDGaldamez <123603754+LuisDGaldamez@users.noreply.github.com>
2025-04-08 21:51:48 -0600

    Add files via upload
    Nav y Footer

---
+++ data/common/navigation.js
@@ -0,0 +1,44 @@
+export const navigation = {
+    navigation: [
+        {
+            url: 'index.html',
+            label: 'Inicio'
+        },
+        {
+            url: 'nosotros.html',
+            label: 'Nosotros'
+        },
+        {
+            url: 'productos.html',
+            label: 'Productos'
+        },
+        {
+            url: 'ayuda.html',
+            label: 'Ayuda'
+        },
+        {
+            url: 'galeria.html',
+            label: 'Galeria'
+        },
+        {
+            url: 'blog.html',
+            label: 'Blog'
+        },
+        {
+            url: 'eventos.html',
+            label: 'Eventos'
+        },
+        {
+            url: 'contactos.html',
+            label: 'Contactenos'
+        },
+        {
+            url: 'testimonios.html',
+            label: 'Testimonios'
+        },
+        {
+            url: 'preguntas.html',
+            label: 'Preguntas Frecuentes'
+        }
+    ]
+}
---
+++ data/indexD.js
@@ -0,0 +1,27 @@
+import { navigation } from "./common/navigation"
+import { footer } from "./common/footer"
+
+import { gallery as indexPageData} from "./pages/gallery"
+
+
+export const getPageContext = (pagePath)=>{
+    console.log("Page to Load Context:", pagePath)
+    const commonVariables = {
+        ...navigation,
+        ...footer
+    }
+
+    let pageVariables = {};
+    console.log("Page been loaded:", pagePath);
+    switch (pagePath) {
+        case '/galeria.html':
+            pageVariables = indexPageData
+            break;
+    }
+    const finalContext = {
+        ...commonVariables,
+        ...pageVariables
+    }
+    console.log("Context: ", JSON.stringify(finalContext, null, 2));
+    return finalContext
+}
