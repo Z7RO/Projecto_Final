a7bf6fbf58a89a2e9bbeba36f5c3502bcec9a78b
LuisZVe <zeronight1220@gmail.com>
2025-04-08 23:38:18 -0600

    Add files via upload

---
+++ data/pages/gallery.js
@@ -0,0 +1,65 @@
+export const gallery = {
+    Establecimientos: [
+        {
+            imgurl: 'imgSource/Establecimiento1.jpg',
+            imgdsc: 'Foto del Establecimiento 1',
+        },
+        {
+            imgurl: 'imgSource/Establecimiento2.jpg',
+            imgdsc: 'Foto del Establecimiento 2',
+        },
+        {
+            imgurl: 'imgSource/Establecimiento3.jpg',
+            imgdsc: 'Foto del Establecimiento 3',
+        },
+        {
+            imgurl: 'imgSource/Establecimiento4.jpg',
+            imgdsc: 'Foto del Establecimiento 4',
+        },
+        {
+            imgurl: 'imgSource/Establecimiento5.jpg',
+            imgdsc: 'Foto del Establecimiento 5',
+        },
+
+    ],
+    Modelos: [
+        {
+            imgurl: 'imgSource/EstilosCollage.jpg',
+            imgdsc: 'Foto Modelo 1',
+        },
+        {
+            imgurl: 'imgSource/EstilosCollage2.jpg',
+            imgdsc: 'Foto Modelo 2',
+        },
+        {
+            imgurl: 'imgSource/EstilosCollage3.jpg',
+            imgdsc: 'Foto Modelo 3',
+        },
+        {
+            imgurl: 'imgSource/EstilosCollage4.jpg',
+            imgdsc: 'Foto Modela 4',
+        },
+        {
+            imgurl: 'imgSource/EstilosCollage5.jpg',
+            imgdsc: 'Foto Modelo 5',
+        },
+        {
+            imgurl: 'imgSource/EstilosCollage6.jpg',
+            imgdsc: 'Foto Modelo 6',
+        },
+    ],
+    PersonalCapacitado: [
+        {
+            imgurl: 'imgSource/PersonalCapacitado1.jpg',
+            imgdsc: 'Foto del Personal 1',
+        },
+        {
+            imgurl: 'imgSource/PersonalCapacitado2.jpg',
+            imgdsc: 'Foto del Personal 2',
+        },
+        {
+            imgurl: 'imgSource/PersonalCapacitado3.jpg',
+            imgdsc: 'Foto del Personal 3',
+        },
+    ],
+};
