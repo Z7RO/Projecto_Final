6a3eb2cebad063732847b9e14778195e93b875f5
LilianBarahona <163463816+LilianBarahona@users.noreply.github.com>
2025-04-09 00:30:09 -0600

    eventos.js

---
+++ data/pages/eventos.js
@@ -0,0 +1,46 @@
+export const eventos = [
+    {
+        titulo: '¡Liquidación de Verano!',
+        descripcion: 'Grandes descuentos en ropa ligera, trajes de baño y accesorios para disfrutar de la temporada.',
+        fecha: '30 de Abril de 2025',
+        mes: 'Abril'
+    },
+    {
+        titulo: 'Especial del Día de la Madre',
+        descripcion: 'Encuentra el regalo perfecto para mamá con nuestras ofertas especiales.',
+        fecha: '11 de Mayo de 2025',
+        mes: 'Mayo'
+    },
+    {
+        titulo: 'Promoción de Mitad de Año',
+        descripcion: 'Disfruta de descuentos especiales en una amplia variedad de productos.',
+        fecha: '15 de Junio de 2025',
+        mes: 'Junio'
+    },
+    {
+        titulo: 'Grandes Ofertas de Temporada',
+        descripcion: 'No te pierdas nuestros increíbles descuentos por tiempo limitado.',
+        fecha: 'Julio de 2025',
+        mes: 'Julio'
+    },
+    {
+        titulo: 'Especial de Aniversario de la Tienda',
+        descripcion: 'Celebra con nosotros nuestro aniversario con ofertas exclusivas.',
+        fecha: 'Agosto de 2025',
+        mes: 'Agosto'
+    },
+    {
+        titulo: 'Promociones de Fiestas Patrias',
+        descripcion: 'Celebra la independencia con descuentos especiales en productos seleccionados.',
+        fecha: '15 de Septiembre de 2025',
+        mes: 'Septiembre'
+    },
+    {
+        titulo: 'Ofertas de Fin de Año',
+        descripcion: 'Prepara tus compras para las fiestas con nuestros descuentos especiales.',
+        fecha: 'Diciembre de 2025',
+        mes: 'Diciembre'
+    },
+    
+   
+];
