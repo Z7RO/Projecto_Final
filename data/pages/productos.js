42fc95aa85715bf6333590ab1d30577b4964f030
LuisDGaldamez <123603754+LuisDGaldamez@users.noreply.github.com>
2025-04-09 00:56:05 -0600

    Productosjs

---
+++ data/pages/productos.js
@@ -0,0 +1,59 @@
+export const productos = {
+    productos: [
+        {
+        imgurl: '../imgSource/Pantalones.jpg',
+        imgdsc: 'Foto Producto 1',
+        titulo: 'Pantalones Levys',
+        descripcion: 'Precio 1328 lps.'
+        },
+        {
+            imgurl: '../imgSource/CamisasC.jpg',
+            imgdsc: 'Foto Producto 2',
+            titulo: 'Camisas Columbia',
+            descripcion: 'Precio 850 lps.'
+        },
+        {
+            imgurl: '../imgSource/CamisasP.jpg',
+            imgdsc: 'Foto Producto 3',
+            titulo: 'Camisas Estilo Polo',
+            descripcion: 'Precio 455 lps.'
+        },
+        {
+            imgurl: '../imgSource/PantalonesP.jpg',
+            imgdsc: 'Foto Producto 4',
+            titulo: 'Pantaloes Pepe Jeans',
+            descripcion: 'Precio 980 lps.'
+        },
+        {
+            imgurl: '../imgSource/GorrasLA.jpg',
+            imgdsc: 'Foto Producto 5',
+            titulo: 'Gorra LA',
+            descripcion: 'Precio 750 lps.'
+        },
+        {
+            imgurl: '../imgSource/TenisO.jpg',
+            imgdsc: 'Foto Producto 6',
+            titulo: 'Tenis On Cloud',
+            descripcion: 'Precio  2300 lps.'
+        },        {
+            imgurl: '../imgSource/GorrasME.jpg',
+            imgdsc: 'Foto Producto 7',
+            titulo: 'Gorra Mercedes Benz',
+            descripcion: 'Precio  970 lps.'
+        },
+        {
+            imgurl: '../imgSource/GorrasNY.jpg',
+            imgdsc: 'Foto Producto 8',
+            titulo: 'Gorra NewYork',
+            descripcion: 'Precio  840 lps.'
+        },
+        {
+            imgurl: '../imgSource/TenisN.jpg',
+            imgdsc: 'Foto Producto 9',
+            titulo: 'Tenis Nike',
+            descripcion: 'Precio  1800 lps.'
+        }
+        
+
+    ]   
+}
