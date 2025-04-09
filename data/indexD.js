c7de2db9fb3c8400f79ba17c4595ab414ab7c481
LuisDGaldamez <123603754+LuisDGaldamez@users.noreply.github.com>
2025-04-09 00:57:53 -0600

    Update indexD.js

--- data/indexD.js
+++ data/indexD.js
@@ -2,14 +2,16 @@ import { navigation } from "./common/navigation"
 import { footer } from "./common/footer"
 import { gallery as indexPageData} from "./pages/gallery"
 import { Preguntas as preguntasPageData } from "./pages/preguntas"
-import { eventos } from "./pages/eventos.js";
-import { eventosP } from "./pages/eventosp.js";
+import { eventos } from "./pages/eventos.js"
+import { eventosP } from "./pages/eventosp.js"
+import { productos } from "./pages/productos.js"
 
 export const getPageContext = (pagePath) => {
     console.log("Page to Load Context:", pagePath)
     const commonVariables = {
         ...navigation,
-        ...footer
+        ...footer,
+        ...productos
     }
 
     let pageVariables = {};
