778662c3b1e7035a49d897f4abe338988846db0f
LuisDGaldamez <123603754+LuisDGaldamez@users.noreply.github.com>
2025-04-09 01:41:49 -0600

    Add files via upload

---
+++ data/pages/testimonios.js
@@ -0,0 +1,76 @@
+export const testimonios = {
+    testimonios: [
+        {
+            titulo: 'Juan Carlos Bodoque',
+            descripcion: 'En Francisco encontré los mejores chalecos para mis reportajes. Calidad periodística y estilo inigualable. ¡5 estrellas!'
+        },
+        {
+            titulo: 'Tulio Triviño',
+            descripcion: 'La ropa de Francisco es tan elegante como mi conducción. ¡Hasta mi amigo Juanín se compró una corbata aquí!'
+        },
+        {
+            titulo: 'Juanín Juan Harry',
+            descripcion: 'Alta facha que me tiro después de comprar en Tiendas Francisco'
+        },
+        {
+            titulo: 'Patana Tufillo',
+            descripcion: 'Odio muchas cosas, pero AMO los vestidos de Francisco. Hasta el Calcetín Roto usaría algo de aquí... si tuviera dinero.'
+        },
+        {
+            titulo: 'Mario Hugo',
+            descripcion: '¡La colección otoño-invierno de Francisco es más impactante que mis coreografías! Superó mis expectativas dramáticamente.'
+        },
+        {
+            titulo: 'Bodoque Bismarck',
+            descripcion: 'Compré un abrigo aquí y ahora hasta los guanacos me piden consejos de moda. ¡Francisco es leyenda!'
+        },
+        {
+            titulo: 'Guarén Romántico',
+            descripcion: 'Sus camisas son tan suaves como mis baladas. Perfectas para cantarle al amor... y a las gangas.'
+        },
+        {
+            titulo: 'Policarpo Avendaño',
+            descripcion: 'Evalué objetivamente: precios 10/10, calidad 10/10, atención 10/10. Conclusión científica: ¡perfecto!'
+        },
+        {
+            titulo: 'Calixto Cavalera',
+            descripcion: '¡La ropa de Francisco es más heavy que mi música! Hasta los duendes del set usan sus jeans.'
+        },
+        {
+            titulo: 'El Calcetín Con Rombosman',
+            descripcion: 'No tengo dinero, pero si lo tuviera, compraría todo en Francisco. Bueno, al menos un calcetín que combine conmigo.'
+        },
+        {
+            titulo: 'Doña Concha',
+            descripcion: 'Ay, mijito, esta tienda es más buena que mis sopaipillas. ¡Hasta el Profesor Rosselini se compró un traje aquí!'
+        },
+        {
+            titulo: 'Profesor Rosselini',
+            descripcion: 'La calidad textil de Francisco es comparable a mis investigaciones sobre el Yeti. ¡Excelente!'
+        },
+        {
+            titulo: 'Raúl Guantecillo',
+            descripcion: 'Soy un hombre simple: veo ropa barata y de calidad, compro. Francisco cumple.'
+        },
+        {
+            titulo: 'Mico el Micófono',
+            descripcion: '¡Wuuu! ¡La ropa de Francisco es tan brillante como mis ideas!'
+        },
+        {
+            titulo: 'Bombón',
+            descripcion: '¡Guau! ¡Los suéteres de Francisco son tan cómodos que hasta me dan ganas de ladrar de felicidad!'
+        },
+        {
+            titulo: 'Policía Checho',
+            descripcion: 'Ordené un uniforme aquí y ahora hasta los ladrones me piden consejos de estilo.¡Aprobado!'
+        },
+        {
+            titulo: 'Policía Chacón',
+            descripcion: 'Confirmo lo de mi compañero Checho. Además, los precios son tan buenos que hasta un policía puede comprar.'
+        },
+        {
+            titulo: 'Tio Pelado',
+            descripcion: 'Entré para robarme unas prendas, pero al ser atendido tan cordialmente me dió penita y decidí pagar por todo.'
+        }
+    ]
+}
