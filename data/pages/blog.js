5bdb25d8fd7ea2bf38899cbee5339421c1743dd4
LilianBarahona <163463816+LilianBarahona@users.noreply.github.com>
2025-04-09 01:29:09 -0600

    blog.js

---
+++ data/pages/blog.js
@@ -0,0 +1,37 @@
+export const entradasBlogTienda = [
+    {
+        titulo: 'Cómo elegir la talla perfecta de tu nueva camiseta',
+        contenido: `Elegir la talla correcta de una camiseta puede ser complicado, pero con nuestra guía detallada, ¡no tendrás problemas! Primero, necesitarás una cinta métrica flexible. Mide la circunferencia de tu pecho, justo por debajo de las axilas y alrededor de la parte más ancha. Asegúrate de que la cinta esté ajustada pero no apretada. Luego, consulta nuestra tabla de tallas específica para cada camiseta, ya que las medidas pueden variar ligeramente entre diferentes estilos y marcas. Si estás entre dos tallas, generalmente recomendamos elegir la más grande para mayor comodidad o si prefieres un ajuste más holgado. ¡Consulta las imágenes ilustrativas para ver dónde medir exactamente!`,
+        fecha: '5 de Abril de 2025',
+        slug: 'como-elegir-talla-perfecta-camiseta',
+        imagen: '/imgSource/blog1.jpg'
+    },
+    {
+        titulo: 'Nuevos accesorios de verano ¡Dale un toque único a tu estilo!',
+        contenido: `Este verano, atrévete a destacar con nuestra increíble colección de accesorios. Desde gafas de sol de última tendencia que te protegerán del sol con estilo, hasta bolsos de playa espaciosos y con diseños vibrantes para llevar todo lo esencial. No olvides nuestros sombreros y gorras que no solo te darán un toque chic, sino que también te protegerán del calor. Explora nuestras pulseras, collares y pendientes con motivos veraniegos como conchas y estrellas de mar. ¡Cada detalle cuenta para crear un look inolvidable!`,
+        fecha: '01 de Abril de 2025',
+        slug: 'nuevos-accesorios-verano-estilo-unico',
+        imagen: '/imgSource/blog2.jpg'
+    },
+    {
+        titulo: '5 consejos para cuidar tus zapatillas de cuero',
+        contenido: `Tus zapatillas de cuero son una inversión que puede durar años si las cuidas adecuadamente. Aquí te dejamos 5 consejos esenciales: 1. Limpia la suciedad superficial regularmente con un paño húmedo. 2. Utiliza productos específicos para el cuidado del cuero para hidratarlo y prevenir grietas. 3. Evita exponerlas a la humedad extrema y, si se mojan, déjalas secar al aire libre, lejos de fuentes directas de calor. 4. Almacénalas en un lugar fresco y seco, preferiblemente con hormas para mantener su forma. 5. Alterna el uso de tus zapatillas de cuero para permitir que se aireen entre puestas. ¡Siguiendo estos consejos, tus zapatillas lucirán siempre impecables!`,
+        fecha: '28 de Marzo de 2025',
+        slug: '5-consejos-cuidar-zapatillas-cuero',
+        imagen: '/imgSource/blog3.jpg'
+    },
+    {
+        titulo: 'La guía definitiva de regalos para el Día de la Madre',
+        contenido: `Se acerca el Día de la Madre y encontrar el regalo perfecto puede ser un desafío. ¡No te preocupes!. Si tu mamá es amante de la moda, nuestra selección de bufandas de seda y joyería elegante seguro le encantará. Para las mamás que disfrutan del hogar, considera una manta suave y acogedora . Si busca relajación, un set de cuidado personal con aceites esenciales será un acierto. ¡Explora nuestras sugerencias y encuentra el regalo ideal para celebrar a esa persona tan especial!`,
+        fecha: '25 de Marzo de 2025',
+        slug: 'guia-definitiva-regalos-dia-de-la-madre',
+        imagen: '/imgSource/blog4.jpg'
+    },
+    {
+        titulo: 'Tendencias de primavera-verano 2025: ¡Renueva tu armario!',
+        contenido: `La nueva temporada trae consigo colores vibrantes y estilos frescos. Prepárate para llenar tu armario con tonos pastel, estampados florales y prendas ligeras. Los vestidos fluidos y las faldas midi serán protagonistas, combinados con sandalias cómodas y alpargatas. No te olvides de los accesorios clave como bolsos de mimbre y gafas de sol oversized. Para los hombres, los pantalones cortos de lino, las camisas de manga corta con estampados tropicales y las zapatillas deportivas serán imprescindibles. ¡Descubre todas las tendencias que te harán lucir a la moda esta temporada!`,
+        fecha: '20 de Marzo de 2025',
+        slug: 'tendencias-primavera-verano-2025',
+        imagen: '/imgSource/blog5.jpg'
+    }
+];
